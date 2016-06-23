688771f4f9f25cdbc136b046247fd823209bd965
guan jiangyu <ggslove@gmail.com>
2016-06-23 10:17:03 +0800

    修改 页面路径，使用bootstrapController来预览界面

--- src/main/java/com/zhili/config/mybatis/BootstrapController.java
+++ src/main/java/com/zhili/config/mybatis/BootstrapController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/manager")
-public class HtmlController {
+@RequestMapping("/bootstrap")
+public class BootstrapController {
 
 	@RequestMapping(value = "/{reader}", method = RequestMethod.GET)
 	public String view(@PathVariable("reader") String reader, Model model) {
-		return "manager/" + reader;
+		return "bootstrap/" + reader;
 	}
 
 }
